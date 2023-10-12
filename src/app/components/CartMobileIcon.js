bca872079c2f855b4f80a859f03993bb58f6c83c
TrungDuc991 <dotrungduc.bl@gmail.com>
2023-10-12 12:40:16 +0700

    count cart mobile

--- src/app/components/CartMobileIcon.js
+++ src/app/components/CartMobileIcon.js
@@ -4,7 +4,7 @@ import { CartContext } from "../context/CartContext";
 import { useContext } from "react";
 
 const CartMobileIcon = () => {
-  const { isOpen, setIsOpen } = useContext(CartContext);
+  const { isOpen, setIsOpen, cart } = useContext(CartContext);
 
   return (
     <div
@@ -12,9 +12,11 @@ const CartMobileIcon = () => {
       className="bg-tertiary w-[72px] h-[72px] rounded-full flex justify-center items-center text-white cursor-pointer  fixed right-[10%] bottom-[5%] z-20 lg:hidden"
     >
       <BsHandbagFill className="text-4xl" />
-      <span className="absolute text bottom-3 right-4 gradient w-5 h-5 flex justify-center items-center rounded-full font-robotoCondensed text-[13px]">
-        3
-      </span>
+      {cart.length > 0 && (
+        <span className="absolute text bottom-3 right-4 gradient w-5 h-5 flex justify-center items-center rounded-full font-robotoCondensed text-[13px]">
+          {cart.length}
+        </span>
+      )}
     </div>
   );
 };
